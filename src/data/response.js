86609b662ac43b0bcc49a308f427c6a2d5255b3c
Helder Oliveira <heldrida@gmail.com>
2020-02-05 11:33:40 +0000

    copied sample data as importables

---
+++ src/data/response.js
@@ -0,0 +1,14 @@
+module.exports = {
+	"response": [
+		{
+			"name": "Molly",
+			"count": 12,
+			"thumbnail": "https://example.com/64x64.png"
+		},
+		{
+			"name": "Polly",
+			"count": 4,
+			"thumbnail": "https://example.com/64x64.png"
+		}
+	]
+}
