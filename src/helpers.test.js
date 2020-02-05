7f313700a2e95ecef8e3df9b377ce5014fecc6d5
Helder Oliveira <heldrida@gmail.com>
2020-02-05 11:44:08 +0000

    include test case for image validation

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,8 +1,17 @@
 const request = require('./data/request.js')
 const response = require('./data/response.js')
-const { jsonPostHandler } = require('./helpers')
+const { jsonPostHandler, isValidImageSize, extractImageFromValidated } = require('./helpers')
 
 test('name and count match their respective properties from the payload', () => {
   const computedResponse = jsonPostHandler(request.payload)
   expect(computedResponse).toEqual(response)
+})
+
+test('should get logo no larger than 128x128 but no smaller than 64x64', () => {
+  const inputImgSizeTooSmall = '16x16'
+  const inputImgSizeValid = '80x80'
+  const inputImgSizeTooBig = '256x256'
+  expect(isValidImageSize(inputImgSizeTooSmall)).toBe(false)
+  expect(isValidImageSize(inputImgSizeValid)).toBe(true)
+  expect(isValidImageSize(inputImgSizeTooBig)).toBe(false)
 })
